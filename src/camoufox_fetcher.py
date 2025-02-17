dc37cf210d256961ccff9307893ca3e320a1cd19
Josef Prochazka <josef.prochazka@apify.com>
2025-02-17 15:28:23 +0100

    Add version fetcher

---
+++ src/camoufox_fetcher.py
@@ -0,0 +1,23 @@
+from camoufox.pkgman import CamoufoxFetcher, Version
+import requests
+
+class VersionSpecificCamoufoxFetcher(CamoufoxFetcher):
+    def set_specific_version(self, version: Version, url: str):
+        self._version_obj=version
+        self._url=url
+
+    def fetch_latest_releases(self, n=3) -> list[Version]:
+        latest_releases = []
+
+        resp = requests.get(self.api_url, timeout=20)
+        resp.raise_for_status()
+
+        releases = resp.json()
+
+        for release in releases:
+            for asset in release['assets']:
+                if data := self.check_asset(asset):
+                    latest_releases.append(data)
+                    if len(latest_releases) >= n:
+                        break
+        return latest_releases
--- src/main.py
+++ src/main.py
@@ -5,33 +5,41 @@ from apify import Actor
 from crawlee.browsers import BrowserPool
 from crawlee.crawlers import PlaywrightCrawler, PlaywrightCrawlingContext
 
+from src.camoufox_fetcher import VersionSpecificCamoufoxFetcher
 from src.input_handling import ActorInputData
 
 async def main() -> None:
     """Actor main function."""
     async with Actor:
         aid = await ActorInputData.from_input()
-
         camoufox_plugin = aid.camoufox_plugin_class()
-        crawler = PlaywrightCrawler(
-            max_requests_per_crawl=aid.max_requests_per_crawl,
-            max_crawl_depth=aid.max_depth,
-            proxy_configuration=aid.proxy_configuration,
-            request_handler_timeout=aid.request_timeout,
-            # Custom browser pool. This gives users full control over browsers used by the crawler.
-            browser_pool=BrowserPool(plugins=[camoufox_plugin]),
-        )
 
-        crawler._http_client.ignore_http_error_status_codes = {403}
+        fetcher = VersionSpecificCamoufoxFetcher()
+        releases = fetcher.fetch_latest_releases(3)
 
-        @crawler.router.default_handler
-        async def request_handler(context: PlaywrightCrawlingContext) -> None:
-            # Process the request.
-            context.log.info(f'Processing {context.request.url} ...')
-            for i in range(15):
-                image = await context.page.screenshot(full_page = True )
-                kvs= await context.get_key_value_store()
-                await kvs.set_value(f"{context.request.url}",image, content_type='image/png')
-                await asyncio.sleep(aid.sleep_time_before_screenshot)
+        # Start one crawler for each release
+        for release in releases:
+            fetcher.set_specific_version(*release)
+            fetcher.install()
+            crawler = PlaywrightCrawler(
+                max_requests_per_crawl=aid.max_requests_per_crawl,
+                max_crawl_depth=aid.max_depth,
+                proxy_configuration=aid.proxy_configuration,
+                request_handler_timeout=aid.request_timeout,
+                # Custom browser pool. This gives users full control over browsers used by the crawler.
+                browser_pool=BrowserPool(plugins=[camoufox_plugin]),
+            )
 
-        await crawler.run(aid.start_urls)
+            crawler._http_client._ignore_http_error_status_codes = {403}
+
+            @crawler.router.default_handler
+            async def request_handler(context: PlaywrightCrawlingContext) -> None:
+                # Process the request.
+                context.log.info(f'Processing {context.request.url} ...')
+                for i in range(15):
+                    image = await context.page.screenshot(full_page = True )
+                    kvs= await context.get_key_value_store()
+                    await kvs.set_value(f"{context.request.url}",image, content_type='image/png')
+                    await asyncio.sleep(aid.sleep_time_before_screenshot)
+
+            await crawler.run(aid.start_urls)
